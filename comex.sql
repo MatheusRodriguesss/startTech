7efc709a1f2582633ebec179ad0fd73324430b26
Matheus Rodrigues <contato.matheusrodrigues3008@gmail.com>
2024-01-29 08:08:52 -0300

    entrega da sprint 5

---
+++ comex.sql
@@ -0,0 +1,48 @@
+1-
+CREATE DATABASE comex;
+
+2-
+CREATE TABLE categoria
+(
+id bigint AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100)
+);
+
+3-
+CREATE TABLE produto
+(
+id bigint AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(255),
+preco decimal(10,2),
+categoria_id bigint,
+ FOREIGN KEY(categoria_id) references categoria(id)
+);
+
+4-
+CREATE TABLE cliente
+(
+id bigint AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(255)
+);
+
+5-
+CREATE TABLE pedido
+(
+id bigint AUTO_INCREMENT PRIMARY KEY,
+data_pedido DATETIME,
+cliente_id bigint,
+FOREIGN KEY(cliente_id) REFERENCES cliente(id)
+);
+
+6-
+CREATE TABLE item_pedido
+(
+id bigint AUTO_INCREMENT PRIMARY KEY,
+pedido_id bigint,
+produto_id bigint,
+preco_unitario decimal(10,2),
+quantidade integer,
+
+FOREIGN KEY(pedido_id) REFERENCES pedido(id),
+FOREIGN KEY(produto_id) REFERENCES produto(id)
+);
---
+++ sprint5/entregas/consulta.sql
@@ -0,0 +1,25 @@
+SELECT categoria.id AS id_categoria, categoria.nome AS categoria, produto.id AS id_produto, produto.nome AS produto, produto.preco AS preco FROM categoria INNER JOIN produto ON 
+categoria.id = produto.categoria_id ORDER BY categoria.nome, produto.nome;
+
+SELECT
+	cliente.nome AS cliente,
+	categoria.nome AS categoria,
+    produto.nome AS produto,
+    produto.preco AS preco_unitario,
+    item_pedido.quantidade AS quantidade,
+    pedido.data_pedido AS datas
+FROM cliente
+INNER JOIN pedido ON cliente.id = pedido.cliente_id
+INNER JOIN item_pedido ON pedido.id = item_pedido.pedido_id
+INNER JOIN produto ON item_pedido.produto_id = produto.id
+INNER JOIN categoria ON categoria.id = produto.categoria_id
+ORDER BY cliente.nome, categoria.nome;
+
+SELECT 
+	categoria.nome,
+    sum(item_pedido.preco_unitario * item_pedido.quantidade) AS total
+from categoria 
+INNER JOIN produto ON categoria.id = produto.categoria_id
+INNER JOIN item_pedido ON item_pedido.produto_id = produto.id
+GROUP BY categoria.nome
+ORDER BY sum(item_pedido.preco_unitario * item_pedido.quantidade) DESC;
